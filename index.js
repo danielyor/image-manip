fb80335f2bf8fba8b41ccb2fb7163b500a97c296
Daniel Yordanov <j.daniel9126@gmail.com>
2022-03-29 19:52:48 +0300

    Improve js formating

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
 // Handle new image uploading on canvas
-function uploadImage(e){
+function uploadImage(e) {
 	const reader = new FileReader();
-	reader.onload = function(event){
-    	img.onload = function(){
-			[width, height] = [img.width, img.height];
-        	canvas.width = width;
-        	canvas.height = height;
-        	ctx.drawImage(img,0,0, width, height);        	
-     	}
-      	img.src = event.target.result;
+	reader.onload = event => {
+		img.onload = () => original();
+		img.src = event.target.result;
 	}
-	reader.readAsDataURL(e.target.files[0]);     
+	reader.readAsDataURL(e.target.files[0]);
 }
 
 // Add download button
-function downloadCanvas(){  
-    // get canvas data  
-    const image = canvas.toDataURL();  
-  
-    // create temporary link  
-    const tmpLink = document.createElement( 'a' );  
-    tmpLink.download = 'image.png'; // set the name of the download file 
-    tmpLink.href = image;  
-  
-    // temporarily add link to body and initiate the download  
-    document.body.appendChild( tmpLink );  
-    tmpLink.click();  
-    document.body.removeChild( tmpLink );  
+function downloadCanvas() {
+	// get canvas data  
+	const image = canvas.toDataURL();
+
+	// create temporary link  
+	const tmpLink = document.createElement('a');
+	tmpLink.download = 'edited-image.png'; // set name of downloaded file 
+	tmpLink.href = image;
+
+	// temporarily add link to body and initiate download  
+	document.body.appendChild(tmpLink);
+	tmpLink.click();
+	document.body.removeChild(tmpLink);
 }
 
 // Revert to original loaded image...
 function original() {
 	[width, height] = [img.width, img.height];
-    canvas.width = width;
-    canvas.height = height;
+	canvas.width = width;
+	canvas.height = height;
 	ctx.drawImage(img, 0, 0, width, height);
 }
 
@@ -55,14 +50,14 @@ async function loadWasm() {
 	const arraySize = (width * height * 4) >>> 0;
 	const nPages = ((arraySize + 0xffff) & ~0xffff) >>> 16;
 	const memory = new WebAssembly.Memory({ initial: nPages });
-  
+
 	const wasm = await WebAssembly.instantiateStreaming(fetch('./build/optimized.wasm'), {
 		env: {
 			memory, // npm run asbuild:optimized -- --importMemory
 			abort: (_msg, _file, line, column) => console.error(`Abort at ${line}:${column}`),
 			seed: () => new Date().getTime()
-			}
-		});
+		}
+	});
 	return wasm.instance.exports;
 }
 
@@ -73,13 +68,13 @@ function originalImageData() {
 
 function copyData(src, dest) {
 	for (let i = 0; i < src.length; i++)
-    	dest[i] = src[i];
+		dest[i] = src[i];
 }
 
 function writeImageData(imageData, bytes) {
 	const data = imageData.data;
-	for (let i = 0; i < data.length; i++) 
-    	data[i] = bytes[i];
+	for (let i = 0; i < data.length; i++)
+		data[i] = bytes[i];
 
 	ctx.putImageData(imageData, 0, 0);
 }
@@ -102,42 +97,34 @@ img.onload = () => original();
 const imageLoader = document.getElementById('imageLoader');
 imageLoader.addEventListener('change', uploadImage, false);
 
-
 // Filters
-
 document.querySelector('.action.original').onclick = e => {
-  e.preventDefault();
-  original();
+	e.preventDefault();
+	original();
 }
-
 document.querySelector('.action.invert').onclick = e => {
-  e.preventDefault();
-  manipulate('invert');
+	e.preventDefault();
+	manipulate('invert');
 }
-
 document.querySelector('.action.grayscale').onclick = e => {
-  e.preventDefault();
-  manipulate('grayscale');
+	e.preventDefault();
+	manipulate('grayscale');
 }
-
 document.querySelector('.action.basicMonochrome').onclick = e => {
-  e.preventDefault();
-  manipulate('basicMonochrome', [150]);
+	e.preventDefault();
+	manipulate('basicMonochrome', [150]);
 }
-
 document.querySelector('.action.randomMonochrome').onclick = e => {
-  e.preventDefault();
-  manipulate('randomMonochrome', [80]);
+	e.preventDefault();
+	manipulate('randomMonochrome', [80]);
 }
 
 // Manipulations
-
 document.querySelector('.action.incBrightness').onclick = e => {
-  e.preventDefault();
-  manipulate('incBrightness', [4]);
+	e.preventDefault();
+	manipulate('incBrightness', [4]);
 }
-
 document.querySelector('.action.decBrightness').onclick = e => {
-  e.preventDefault();
-  manipulate('decBrightness', [4]);
+	e.preventDefault();
+	manipulate('decBrightness', [4]);
 }
